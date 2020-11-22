8d2cf4ff5aeb80c8e0800216b3385fff14dd378a
Darko Lukić <lukicdarkoo@gmail.com>
2020-11-22 10:53:34 +0100

    Update zeroplus.ino

--- zeroplus/zeroplus.ino
+++ zeroplus/zeroplus.ino
@@ -4,6 +4,8 @@ arduino-cli core install arduino:samd
 
 arduino-cli compile --upload --port /dev/ttyACM0 -b arduino:samd:arduino_zero_native
 picocom -b 115200 /dev/ttyACM0
+
+> If the board is not recognized as a tty device then double click the reset button to load active the bootloader.
 */
 
 void setup()
