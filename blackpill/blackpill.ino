604a0a49d59c8490666b5528571b738b178d7d56
Darko Lukic <lukicdarkoo@gmail.com>
2020-12-28 23:29:04 +0100

    Add STM32Cube

--- blackpill/blackpill.ino
+++ blackpill/blackpill.ino
@@ -1,8 +1,12 @@
 /*
-sudo usermod -a -G dialout $USER
-
 arduino-cli core update-index
 arduino-cli core install STM32:stm32
+
+wget https://www.st.com/content/ccc/resource/technical/software/utility/group0/32/27/b0/4a/01/e2/40/56/stm32cubeprg-lin_v2-6-0/files/stm32cubeprg-lin_v2-6-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-6-0.zip
+tar -xvf en.stm32cubeprg-lin_v2-6-0.zip
+./en.stm32cubeprg-lin_v2-6-0/SetupSTM32CubeProgrammer-2.6.0.linux
+sudo cp /home/lukic/STMicroelectronics/STM32Cube/STM32CubeProgrammer/Drivers/rules/* /etc/udev/rules.d
+sudo udevadm control -R
 
 > press both the boot button and reset buttons, release the reset button, the release the boot button after a half-second
 arduino-cli compile --upload -b STM32:stm32:GenF4:pnum=BLACKPILL_F401CC,upload_method=dfuMethod
